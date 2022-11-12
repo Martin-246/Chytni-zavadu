fddd0fda4e534198413ae2c1a15c0fc79208f688
Siki-ux <xsikul15@stud.fit.vutbr.cz>
2022-11-12 12:45:57 +0100

    added back button

--- present_layer/all_tickets.php
+++ present_layer/all_tickets.php
@@ -8,10 +8,23 @@ h2 {text-align: center;}
 table {align-self: center;}
 img {width:50px;height:50px;}
 td {text-align: center;}
+li {display: inline-block;}
+li > a {padding: 10px;color: #000000 ;text-decoration: none;border: 1px solid #000000;border-radius: 5px;}
+li > a:hover {background-color: #848482;color: #FFFFFFFF}
+li > h2 {padding-left: 300px;}
 </style></head>
 <html>
     <body>
-        <h2>Všetky tikety</h2><br>
+        <nav>
+            <ul id="navbar">
+                <li>
+                    <a href = "../index.php">Späť</a>
+                </li>
+                <li>
+                    <h2>Všetky tikety</h2>
+                </li>
+            </ul>
+        </nav>
         <table style="width:100%">
             <tr>
                 <th>ID</th>
