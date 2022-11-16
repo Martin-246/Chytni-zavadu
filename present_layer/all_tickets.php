7adf3c5c18ef2ae02b0d01b2bff09e528d562b3e
Siki-ux <xsikul15@vutbr.cz>
2022-11-16 13:42:16 +0100

    haha

--- present_layer/all_tickets.php
+++ present_layer/all_tickets.php
@@ -4,6 +4,10 @@ include_once('../bussiness_layer/get_ticket.php');
 
 ?>
 <head><style>
+body{font-family: Arial, Helvetica;
+    margin:0%;
+    font-size: 1rem;
+    min-width: 350px;}
 h2 {text-align: center;}
 table {align-self: center;}
 img {width:50px;height:50px;}
@@ -12,7 +16,11 @@ li {display: inline-block;}
 li > a {padding: 10px;color: #000000 ;text-decoration: none;border: 1px solid #000000;border-radius: 5px;}
 li > a:hover {background-color: #848482;color: #FFFFFFFF}
 li > h2 {padding-left: 300px;}
-</style></head>
+table{border-collapse: collapse;}
+th {border: 1px solid #000000;}
+td {border-bottom: 1px solid #000000;}
+</style>
+</head>
 <html>
     <body>
         <nav>
@@ -38,5 +46,8 @@ li > h2 {padding-left: 300px;}
             </tr>
             <?php print_all_tickets_table_row();?>
         </table>
+        <script>
+            setTimeout(function(){location = ''},5000)
+        </script>
     </body>
 </html>
