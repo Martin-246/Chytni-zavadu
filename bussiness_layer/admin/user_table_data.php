fca0d9eede3c7526e01bce6ef9b20749862fb4d2
Vladyslav T <58009231+44sides@users.noreply.github.com>
2022-11-23 13:31:30 +0100

    fixed my mistake

--- bussiness_layer/admin/user_table_data.php
+++ bussiness_layer/admin/user_table_data.php
@@ -1,24 +1,18 @@
 <?php
     include_once('./data_layer/db_user.php');
     include_once('./bussiness_layer/constants.php');
-
     function foo($id)
     {
         echo "-> ".$id."\n";
     }
-
     function get_user_table_rows()
     {
         global $roles;
-
         $html = "";
-
         $stmt = get_all_users();
-
         while( $row = $stmt->fetch() )
         {
             $html_row = '<tr>';
-
             $html_row .= '<td> '.$row['id']." </td>\n";
             $html_row .= '<td> '.$row['first_name']." </td>\n";
             $html_row .= '<td> '.$row['last_name']." </td>\n";
@@ -28,16 +22,23 @@
 
             // button that removes the user. P.S. Admin cannot be removed
             if($row['role'] != ADMIN)
-                $html_row .= '<td> <button onclick="handle_remove_button('. $row['id'] .')"> Odstrániť </button> </td>' . "\n";
+            {
+                if(user_has_tickets($row['id']))
+                    // User has active tickets. Cannot remove
+                    $html_row .= '<td> Má aktívne tikety </td>'."\n";
+                else if(user_has_service_requests($row['id']))
+                    // User has active service requests. Cannot remove
+                    $html_row .= '<td> Má service requesty </td>'."\n";
+                else
+                    // Generate delete button
+                    $html_row .= '<td> <button onclick="handle_remove_button('. $row['id'] .')"> Odstrániť </button> </td>' . "\n";
+            }
             else
                 $html_row .= "<td> </td>\n";
 
             $html_row .= "<tr>\n";
-
             $html .= $html_row;
         }
-
         return $html;
     }
-
 ?>
