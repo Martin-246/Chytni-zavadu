31945222280f7c15478cc03e75ec6bf820ec740b
Pavella Martin <xpavel39@merlin.fit.vutbr.cz>
2022-11-24 08:47:28 +0100

    anonymous user cannot do stuff now

--- index.php
+++ index.php
@@ -42,48 +42,39 @@ if( is_worker() )
         <br>';
     } else
     {
+        // Render UI for logged in user
         echo("Logged in as: ".$_SESSION['email']);
-    }
-    ?>
-
-    <form action="present_layer/all_tickets.php" class="inline">
-        <button>Vsetky tikety</button>
-    </form>
-
-    <br>
-
-    <form action="present_layer/all_tickets_map.php" class="inline">
-        <button>Mapa</button>
-    </form>
-
-    <br>
-
-    <form action="present_layer/new_ticket.php" class="inline">
-        <button>Nový tiket</button>
-    </form>
-
-    <br>
-
-
-    <?php
-    if( is_logged_in()) // Allow logged in user to log out
-    {
         echo '
+        <form action="present_layer/all_tickets.php" class="inline">
+            <button>Vsetky tikety</button>
+        </form>
+
+        <br>
+
+        <form action="present_layer/all_tickets_map.php" class="inline">
+            <button>Mapa</button>
+        </form>
+
+        <br>
+
+        <form action="present_layer/new_ticket.php" class="inline">
+            <button>Nový tiket</button>
+        </form>
+
+        <br>
+
+
         <form action="present_layer/my_tickets.php" class="inline">
             <button>Moje tikety</button>
         </form>
-
         <br>
-
         <form action="present_layer/authentication/logout.php" class="inline">
             <button>Odhlásiť</button>
         </form>
+        
+        <br>';
+    }
 
-        <br>
-        ';
-    } 
     ?>
-
-
 </html>
 
